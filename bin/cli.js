31be37d45b5d7880d7e9818199730e3544da33b5
Glib Briia <glib.briia@gmail.com>
2024-02-13 22:59:13 +0000

    Refactor params; switch to axios

--- bin/cli.js
+++ bin/cli.js
@@ -7,31 +7,32 @@ const dateFormat = require('dateformat');
 
 program
   .version('1.2.0')
-  .option('-a, --accessKey [ASSERTTHAT_ACCESS_KEY]', 'Access key')
-  .option('-s, --secretKey [ASSERTTHAT_SECRET_KEY]', 'Secret key')
-  .option('-t, --token [ASSERTTHAT_API_TOKEN]', 'Jira API token (Server and DC only)')
-  .option('-u, --jiraServerUrl [URL]', 'Jira server URL e.g https://mycompanyjira.com')
-  .option('-f, --features', 'Download features')
-  .option('-r, --report', 'Upload report')
-  .option('-i, --projectId <ID>', 'Jira project id')
-  .option('-j, --jsonReportFolder [FOLDER PATH]', 'Cucumber json files folder')
-  .option('-m, --mode <mode>', 'Features to download', /^(automated|manual|both)$/i, 'automated')
-  .option('-l, --tags <tag-expression>', 'Cucumber tag expression for filtering scenarios', '')
-  .option('-n, --runName [NAME]', 'Test run name')
-  .option('-d, --metadata [FILE PATH]', 'Metadata json file path')
-  .option('-o, --outputFolder [FOLDER PATH]', 'Jira project id')
-  .option('-q, --jql [JQL]', 'JQL filter for features download and report upload')
-  .option('-p, --jsonReportIncludePattern [PATTERN]', 'Pattern for json file names')
-  .option('-x, --proxyURI [URI]', 'Proxy URI')
-  .option('-b, --numbered [true|false]', 'Append number to feature name on download');
+  .option('--accessKey [ASSERTTHAT_ACCESS_KEY]', 'Access key')
+  .option('--secretKey [ASSERTTHAT_SECRET_KEY]', 'Secret key')
+  .option('--token [ASSERTTHAT_API_TOKEN]', 'Jira API token (Server and DC only)')
+  .option('--jiraServerUrl [URL]', 'Jira server URL e.g https://mycompanyjira.com')
+  .option('--features', 'Download features')
+  .option('--report', 'Upload report')
+  .option('--projectId <ID>', 'Jira project id')
+  .option('--jsonReportFolder [FOLDER PATH]', 'Cucumber json files folder')
+  .option('--mode <mode>', 'Features to download', /^(automated|manual|both)$/i, 'automated')
+  .option('--tags <tag-expression>', 'Cucumber tag expression for filtering scenarios', '')
+  .option('--runName [NAME]', 'Test run name')
+  .option('--metadata [FILE PATH]', 'Metadata json file path')
+  .option('--outputFolder [FOLDER PATH]', 'Jira project id')
+  .option('--jql [JQL]', 'JQL filter for features download and report upload')
+  .option('--jsonReportIncludePattern [PATTERN]', 'Pattern for json file names')
+  .option('--proxyHost [Host]', 'Proxy Host')
+  .option('--proxyPort [Port]', 'Proxy Port')
+  .option('--numbered [true|false]', 'Append number to feature name on download');
 
 program.on('--help', function(){
   console.log('')
   console.log('Examples:');
-  console.log('  $ assertthat-bdd -f -i 10001 -a "access_key" -s "secret_key"');
-  console.log('  $ assertthat-bdd -f -i 10001 -t JIRA_API_TOKEN');
-  console.log('  $ assertthat-bdd -r -i 10001 -a "access_key" -s "secret_key"');
-  console.log('  $ assertthat-bdd -r -i 10001 -t JIRA_API_TOKEN');
+  console.log('  $ assertthat-bdd --features --projectId 10001 --accessKey "access_key" --secretKey "secret_key"');
+  console.log('  $ assertthat-bdd --features --projectId 10001 --token JIRA_API_TOKEN');
+  console.log('  $ assertthat-bdd --report --projectId 10001 --accessKey "access_key" --secretKey "secret_key"');
+  console.log('  $ assertthat-bdd --report --projectId 10001 --token JIRA_API_TOKEN');
   console.log('  $ assertthat-bdd -h');
 });
 
@@ -52,8 +53,8 @@ const settings = {
     jql: program.jql || '',
     tags: program.tags || '',
     jsonReportIncludePattern: program.jsonReportIncludePattern || '.+.json',
-    proxyURI: program.proxyURI || process.env.http_proxy,
-
+    proxyHost: program.proxyHost,
+    proxyPort: program.proxyPort,
 };
 
 function make_red(txt) {
@@ -64,12 +65,12 @@ if(program.features){
     if(!settings.projectId || (!settings.token && (!settings.accessKey || !settings.secretKey))){
         if(!program.projectId){
             console.log('');
-            console.log(make_red('projectId (-i) option is required'));
+            console.log(make_red('--projectId option is required'));
             console.log('');
         }
         if(!settings.token && (!settings.accessKey || !settings.secretKey)){
             console.log('');
-            console.log(make_red('accessKey (-a) with secretKey (-s) or Jira API token (-t) option is required'));
+            console.log(make_red('--accessKey with --secretKey or Jira API --token option is required'));
             console.log('');
         }
         program.outputHelp(make_red);
